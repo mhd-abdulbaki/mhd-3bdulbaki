f888ab3f13ade5c448ab2bd289464380b9716185
Mhd <homamsy64@gmail.com>
2022-01-16 00:51:49 +0200

    up performance

--- JS/cards.js
+++ JS/cards.js
@@ -1,41 +1,41 @@
 repos = [
   {
-    img: "./Images/hulu.png",
+    img: "./Images/hulu.WebP",
     title: "Hulu Homepage",
     des: "Hulu homepage using HTML, CSS techniques such as CSS flex, grid and media query and JavaScript.",
     url: "https://mhd-abdulbaki.github.io/Hulu/",
     githubLink: "https://github.com/mhd-abdulbaki/Hulu/",
   },
   {
-    img: "./Images/social.png",
+    img: "./Images/social.WebP",
     title: "Social Network",
     des: "Design Social Network using Sass and CSS techniques such as CSS flex, grid and media query.",
     url: "https://mhd-abdulbaki.github.io/Social-Network-Theme/dashboard.html",
     githubLink: "https://github.com/mhd-abdulbaki/Social-Network-Theme",
   },
   {
-    img: "./Images/hosting.png",
+    img: "./Images/hosting.WebP",
     title: "Hosting website",
     des: "Hosting website using HTML and CSS techniques such as CSS flex, grid and media query, and JavaScript.",
     url: "https://mhd-abdulbaki.github.io/Hosting-website/",
     githubLink: "https://github.com/mhd-abdulbaki/Hosting-website",
   },
   {
-    img: "./Images/software.png",
+    img: "./Images/software.WebP",
     title: "Software Service homepage",
     des: "Full responsive design, with awesome full screen image slider using HTML, CSS and Javascript.",
     url: "https://mhd-abdulbaki.github.io/software-service/",
     githubLink: "https://github.com/mhd-abdulbaki/software-service",
   },
   {
-    img: "./Images/3d-card.png",
+    img: "./Images/3d-card.WebP",
     title: "3D Card Animation",
     des: "Awesome 3D Card Animation using vanilla Javascript",
     url: "https://mhd-abdulbaki.github.io/Awesome-3D-Card-/",
     githubLink: "https://github.com/mhd-abdulbaki/Awesome-3D-Card-",
   },
   {
-    img: "./Images/business.png",
+    img: "./Images/business.WebP",
     title: "Business website",
     des: "Full responsive design, using HTML, CSS techniques such as CSS flex, grid and media query and Javascript.",
     url: "https://mhd-abdulbaki.github.io/Business-website/",
@@ -51,8 +51,8 @@ repos.forEach((repo) => {
         <h6 class="card-title">${repo.title}</h6>
         <p class="card-intro">${repo.des}</p>
         <div class="info">
-          <a class = 'btn' href=${repo.githubLink} target="_blank"><i class="fab fa-github"></i>  Github</a>
-          <a class = 'btn' href=${repo.url} target="_blank" ><i class="fas fa-eye"></i> visit</a>
+          <a class = 'btn' rel="noreferrer" href=${repo.githubLink} target="_blank"><i class="fab fa-github"></i>  Github</a>
+          <a class = 'btn' rel="noreferrer" href=${repo.url} target="_blank" ><i class="fas fa-eye"></i> visit</a>
         </div>
     </div>`;
 });
